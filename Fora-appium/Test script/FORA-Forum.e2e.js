ed45a384de145f22e310b3783daf56349068c310
Surya <surya@qaoncloud.com>
2024-03-19 10:24:13 +0530

    19 march script update

--- Fora-appium/Test script/FORA-Forum.e2e.js
+++ Fora-appium/Test script/FORA-Forum.e2e.js
@@ -1,41 +1,76 @@
 const { expect, browser, $ } = require("@wdio/globals");
-const { remote } = require('webdriverio');
-const assert = require('assert');
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { remote } = require("webdriverio");
+const assert = require("assert");
+const { Builder, By, Key, until } = require("selenium-webdriver");
 
 describe("fora", () => {
-  
-
   it("Advisor_Foraemail_TC001", async () => {
-    // login 
     await browser.url("https://advisor.forastaging.net/");
-    const signInButton = await $("//button[normalize-space()='Sign in with your Fora email']")
-    await signInButton.click()   
-    //enter valid email ID
-    const emailinputField = await $("//input[@id='identifierId']")
-    await emailinputField.setValue("mqa.mqa020201@forastaging.net")
-    emailinputField.sendKeyEvent(66)
-   const nextButton = await $(".VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.qIypjc.TrZEUc.lw1w4b")
-   await nextButton.click();
-   //enter valid password
-   const password = await $("//input[@name='Passwd']");
-   await password.setValue("Qaoncloud@01");
-   await nextButton.click();
-   const continueBtn = await $("//body[1]/div[1]/div[1]/div[2]/div[1]/c-wiz[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/button[1]")
-   await continueBtn.click();
-   await browser.pause(10000); // pause for 10 seconds
+    const signInButton = await $(
+      "//button[normalize-space()='Sign in with your Fora email']"
+    );
+    await signInButton.click();
+    await browser.pause(10000);
 
-   //welcome mqa!
-   const welcometext = await $("//h1[normalize-space()='Welcome, mqa!']")
-   if(!welcometext.isDisplayed()) {
-   throw new Error("Welcome text not displayed");
-   }
-  //click menu and click RESOURCES
-  console.log("click menu ");
-  const menu = await $("//*[@id=\"navbar-open\"]")
-  menu.click()
-  const forum = await $("//span[normalize-space()='Forum']")
-  forum.click()
- 
-})
-})
+    const emailinputField = await $("//input[@id='identifierId']");
+    await emailinputField.setValue("mqa.mqa020201@forastaging.net");
+    await browser.pause(2000);
+
+    const nextButton = await $('//*[@id="identifierNext"]/div/button/span');
+    await nextButton.click();
+    await browser.pause(2000);
+
+    const password = await $("//input[@name='Passwd']");
+    await password.setValue("Qaoncloud@01");
+    await browser.pause(3000);
+    //const nextButton2 = await $("//*[@id=\"passwordNext\"]/div/button/div[3]")
+    const nextButton2 = await $("//span[normalize-space()='Next']");
+    await nextButton2.click();
+    await browser.pause(2000);
+    const continueBtn = await $("//span[normalize-space()='Continue']");
+    await continueBtn.click();
+    await browser.pause(10000);
+    //welcome mqa!
+    const welcometext = await $("//h1[normalize-space()='Welcome, mqa!']");
+    if (!welcometext.isDisplayed()) {
+      throw new Error("Welcome text not displayed");
+    }
+
+    //click menu and click forum
+    console.log("click menu ");
+    const menu = await $('//*[@id="navbar-open"]');
+    menu.click();
+    const forum = await $("//span[normalize-space()='Forum']");
+    forum.click();
+
+    await browser.pause(2000);
+    const foratitle = await $("//img[@alt='Fora Forum']");
+    if (!foratitle.isDisplayed()) {
+      throw new Error("foratitle  not displayed");
+    }
+    const login = await $("//h1[normalize-space()='Log in to your account']");
+    if (!login.isDisplayed()) {
+      throw new Error("login  not displayed");
+    }
+    const continue1 = await $(
+      "//button[contains(text(),'Continue with your fora.travel email single sign-o')]"
+    );
+    if (!continue1.isDisplayed()) {
+      throw new Error(
+        "Continue with your fora.travel email single sign-on  not displayed"
+      );
+    }
+    const legacy = await $(
+      "//button[contains(text(),'Legacy Login: Sign in using your existing email & ')]"
+    );
+    if (!legacy.isDisplayed()) {
+      throw new Error("legacy  not displayed");
+    }
+    const forgotpass = await $(
+      "//a[normalize-space()='Forgot your password?']"
+    );
+    if (!forgotpass.isDisplayed()) {
+      throw new Error("legacy  not displayed");
+    }
+  });
+});
--- Fora-appium/Test script/FORA-RESOURCES.e2e.js
+++ Fora-appium/Test script/FORA-RESOURCES.e2e.js
@@ -1,80 +1,86 @@
 const { expect, browser, $ } = require("@wdio/globals");
-const { remote } = require('webdriverio');
-const assert = require('assert');
-const { Builder, By, Key, until } = require('selenium-webdriver');
+const { remote } = require("webdriverio");
+const assert = require("assert");
+const { Builder, By, Key, until } = require("selenium-webdriver");
 
 describe("fora", () => {
-  
-
   it("Advisor_Resources_TC001", async () => {
-    // login 
     await browser.url("https://advisor.forastaging.net/");
-    const signInButton = await $("//button[normalize-space()='Sign in with your Fora email']")
-    await signInButton.click()   
-    //enter valid email ID
-    const emailinputField = await $("//input[@id='identifierId']")
-    await emailinputField.setValue("mqa.mqa020201@forastaging.net")
-    emailinputField.sendKeyEvent(66)
-   const nextButton = await $(".VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.LQeN7.qIypjc.TrZEUc.lw1w4b")
-   await nextButton.click();
-   //enter valid password
-   const password = await $("//input[@name='Passwd']");
-   await password.setValue("Qaoncloud@01");
-   await nextButton.click();
-   const continueBtn = await $("//body[1]/div[1]/div[1]/div[2]/div[1]/c-wiz[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/button[1]")
-   await continueBtn.click();
-   await browser.pause(10000); // pause for 10 seconds
+    const signInButton = await $(
+      "//button[normalize-space()='Sign in with your Fora email']"
+    );
+    await signInButton.click();
+    await browser.pause(10000);
 
-   //welcome mqa!
-   const welcometext = await $("//h1[normalize-space()='Welcome, mqa!']")
-   if(!welcometext.isDisplayed()) {
-   throw new Error("Welcome text not displayed");
-   }
-  //click menu and click RESOURCES
-  console.log("click menu ");
-  const menu = await $("//*[@id=\"navbar-open\"]")
-  menu.click()
-  const resource = await $("//span[normalize-space()='Resources']")
-  resource.click()
-  await browser.pause(10000); // pause for 10 seconds
-  //resource title
-   const resourcetitle = await $("//h1[normalize-space()='Resources']")
-   if(!resourcetitle.isDisplayed()) {
-   throw new Error("resource title not displayed");
-   }
+    const emailinputField = await $("//input[@id='identifierId']");
+    await emailinputField.setValue("mqa.mqa020201@forastaging.net");
+    await browser.pause(2000);
 
-   //email template
-   const emailtemp = await $("//p[normalize-space()='Email templates']")
-   if(!emailtemp.isDisplayed()) {
-   throw new Error("resource title not displayed");
-   }
+    const nextButton = await $('//*[@id="identifierNext"]/div/button/span');
+    await nextButton.click();
+    await browser.pause(2000);
 
-   //Itinerary
-   const Itinerary = await $("//p[normalize-space()='Itinerary template']")
-   if(!Itinerary.isDisplayed()) {
-   throw new Error("Itinerary not displayed");
-   }
+    const password = await $("//input[@name='Passwd']");
+    await password.setValue("Qaoncloud@01");
+    await browser.pause(3000);
+    //const nextButton2 = await $("//*[@id=\"passwordNext\"]/div/button/div[3]")
+    const nextButton2 = await $("//span[normalize-space()='Next']");
+    await nextButton2.click();
+    await browser.pause(2000);
+    const continueBtn = await $("//span[normalize-space()='Continue']");
+    await continueBtn.click();
+    await browser.pause(10000);
+    //welcome mqa!
+    const welcometext = await $("//h1[normalize-space()='Welcome, mqa!']");
+    if (!welcometext.isDisplayed()) {
+      throw new Error("Welcome text not displayed");
+    }
 
-   //Forms
-   const Forms = await $("//p[normalize-space()='Forms']")
-   if(!Forms.isDisplayed()) {
-   throw new Error("Forms not displayed");
-   }
+    //click menu and click RESOURCES
+    console.log("click menu ");
+    const menu = await $('//*[@id="navbar-open"]');
+    menu.click();
+    const resource = await $("//span[normalize-space()='Resources']");
+    resource.click();
+    await browser.pause(10000); // pause for 10 seconds
+    //resource title
+    const resourcetitle = await $("//h1[normalize-space()='Resources']");
+    if (!resourcetitle.isDisplayed()) {
+      throw new Error("resource title not displayed");
+    }
 
-   //Other
-   const Other = await $("//p[normalize-space()='Other resources']")
-   if(!Other.isDisplayed()) {
-   throw new Error("Other not displayed");
-   }
+    //email template
+    const emailtemp = await $("//p[normalize-space()='Email templates']");
+    if (!emailtemp.isDisplayed()) {
+      throw new Error("resource title not displayed");
+    }
 
- 
-})
-it("Advisor_Resources_TC002", async () => {
-//open in google drive - email template
-const email = await $("//*[@id=\"main-container\"]/main/div/div[1]/div[2]/dl/div[1]/div/a/span")
-email.click()
-await browser.pause(10000);
-/*const selectaccnt = await $("//*[@id=\"com.google.android.apps.docs.editors.docs:id/alertTitle\"]")
+    //Itinerary
+    const Itinerary = await $("//p[normalize-space()='Itinerary template']");
+    if (!Itinerary.isDisplayed()) {
+      throw new Error("Itinerary not displayed");
+    }
+
+    //Forms
+    const Forms = await $("//p[normalize-space()='Forms']");
+    if (!Forms.isDisplayed()) {
+      throw new Error("Forms not displayed");
+    }
+
+    //Other
+    const Other = await $("//p[normalize-space()='Other resources']");
+    if (!Other.isDisplayed()) {
+      throw new Error("Other not displayed");
+    }
+  });
+  it("Advisor_Resources_TC002", async () => {
+    //open in google drive - email template
+    const email = await $(
+      '//*[@id="main-container"]/main/div/div[1]/div[2]/dl/div[1]/div/a/span'
+    );
+    email.click();
+    await browser.pause(10000);
+    /*const selectaccnt = await $("//*[@id=\"com.google.android.apps.docs.editors.docs:id/alertTitle\"]")
 if(!selectaccnt.isDisplayed()) {
     throw new Error("select accnt not displayed");
     }
@@ -84,31 +90,33 @@ if(!selectaccnt.isDisplayed()) {
 const cancel = await $("//button[@id='android:id/button2']");
 
 cancel.click() */
-await browser.pause(10000);
-})
+    await browser.pause(3000);
+  });
 
-it("Advisor_Resources_TC003", async () => {
+  it("Advisor_Resources_TC003", async () => {
     //open in google drive - Itinerary
-    const Itinerary = await $("//*[@id=\"main-container\"]/main/div/div[1]/div[2]/dl/div[2]/div/a/span")
-    Itinerary.click() 
-    await browser.pause(10000);
+    const Itinerary = await $(
+      '//*[@id="main-container"]/main/div/div[1]/div[2]/dl/div[2]/div/a/span'
+    );
+    Itinerary.click();
+    await browser.pause(3000);
+  });
 
-})
-
-it("Advisor_Resources_TC004", async () => {
+  it("Advisor_Resources_TC004", async () => {
     //open in google drive - Forms
-    const Forms = await $("//*[@id=\"main-container\"]/main/div/div[1]/div[2]/dl/div[3]/div/a/span")
-    Forms.click()
-    await browser.pause(10000);
+    const Forms = await $(
+      '//*[@id="main-container"]/main/div/div[1]/div[2]/dl/div[3]/div/a/span'
+    );
+    Forms.click();
+    await browser.pause(3000);
+  });
 
-})
-
-it("Advisor_Resources_TC005", async () => {
+  it("Advisor_Resources_TC005", async () => {
     //open in google drive - Other
-    const Other = await $("//*[@id=\"main-container\"]/main/div/div[1]/div[2]/dl/div[4]/div/a/span")
-    Other.click()
-    await browser.pause(10000);
-
-})
-
-})
+    const Other = await $(
+      '//*[@id="main-container"]/main/div/div[1]/div[2]/dl/div[4]/div/a/span'
+    );
+    Other.click();
+    await browser.pause(3000);
+  });
+});
